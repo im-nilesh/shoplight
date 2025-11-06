e93bf83d7b6c7faefbdd5b410bd1e39b9bb86e9a
Nilesh <nileshdadab@gmail.com>
2025-11-06 12:33:23 +0530

    Setting up the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen
+          name="notification"
+          options={{ title: "Notifications" }}
+        />
+      </Stack>
+    </SafeAreaProvider>
+  );
 }
